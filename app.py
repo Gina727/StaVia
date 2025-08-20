7e4843e59201637eeb5c60f3c433dc66dd9fdda7
Pearnoi <pitchayapa08@gmail.com>
2025-08-20 08:22:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -410,4 +410,4 @@ def download_all():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=False)
+    app.run((host='0.0.0.0', debug=True, use_reloader=False)

5a0db3d696bcd9d9fa85e33a1f0f00231ace2683
Gina727 <chienyuehliu@gmail.com>
2025-10-30 11:11:39 +0800

    modified:   file_functions.py

--- file_functions.py
+++ file_functions.py
@@ -2,45 +2,6 @@ import pandas as pd
 import scanpy as sc
 import os
 
-# def read_10x_mtx(file_path):
-#     import scipy.io
-
-#     matrix_path = os.path.join(file_path, 'matrix.mtx.gz')
-#     print(f"Reading matrix from: {matrix_path}")
-#     matrix = scipy.io.mmread(matrix_path)
-#     print(f"Matrix shape: {matrix.shape}")
-    
-#     # read barcodes
-#     barcodes_path = os.path.join(file_path, 'barcodes.tsv.gz')
-#     print(f"Reading barcodes from: {barcodes_path}")
-#     barcodes = pd.read_csv(barcodes_path, header=None, sep='\t')[0].values
-#     print(f"Barcodes count: {len(barcodes)}")
-    
-#     # read features
-#     features_path = os.path.join(file_path, 'features.tsv.gz')
-#     print(f"Reading features from: {features_path}")
-#     features = pd.read_csv(features_path, header=None, sep='\t')
-#     print(f"Features shape: {features.shape}")
-#     print(f"Features columns: {features.columns.tolist()}")
-    
-#     # create AnnData
-#     adata = sc.AnnData(X=matrix.T.tocsr())
-#     adata.obs_names = barcodes
-#     # if features has at least 2 column, set 2nd column as gene_id
-#     if features.shape[1] >= 2:
-#         adata.var_names = features[1].values
-#         adata.var['gene_ids'] = features[0].values
-#     else:
-#         adata.var_names = features[0].values
-    
-#     # if feature has at least 3 columns, set 3rd column as feature_types
-#     if features.shape[1] >= 3:
-#         adata.var['feature_types'] = features[2].values
-    
-#     print(f"obs: {adata.obs.keys()}")
-#     return adata
-
-
 def read_10x_mtx(file_path):
     import scipy.io
     import pandas as pd
